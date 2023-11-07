137528b495ab3a73bea63e4802ec5dcd6258115d
camilagsoares <camila.soares19@hotmail.com>
2023-11-07 20:04:30 -0300

    Lab 04

---
+++ api_social_books/src/main/java/br/fepi/socialbooks/repository/LivrosRepository.java
@@ -0,0 +1,7 @@
+package br.fepi.socialbooks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivrosRepository extends JpaRepository<Livro>{
+	
+}
--- api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -3,17 +3,23 @@ package br.fepi.socialbooks.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.fepi.socialbooks.domain.Livro;
+import br.fepi.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping(value = "/livros")
 public class LivrosResources {
 	
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@Autowired
+	private LivrosRepository livrosRepository;
 	
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro>  listar() {
 		Livro livro1 = new Livro("a arte da guerra");
 		Livro livro2 = new Livro("O pequeno príncipe");
@@ -24,6 +30,19 @@ public class LivrosResources {
 		};
 		
 		return Arrays.asList(livros);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		
+		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)	
+	public Livro buscaId(@PathVariable("id") long id) {
+		
+		
+		return livrosRepository.findById(id).orElse(null);
 	}
 	
 }
