1818f364df3988071dde31c61ea0fe1c83bfbb84
Camila Goulart Soares <00019305@fepi.local>
2023-11-29 19:28:15 -0300

    Lab 05

--- api_social_books/src/main/java/br/fepi/socialbooks/domain/Livro.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/domain/Livro.java
@@ -1,13 +1,11 @@
 package br.fepi.socialbooks.domain;
 import java.util.Date;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
