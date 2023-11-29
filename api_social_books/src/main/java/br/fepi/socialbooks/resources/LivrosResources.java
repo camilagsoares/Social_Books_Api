1569953f8b781ba10bd57325bce15e5f62a6cc46
Camila Goulart Soares <00019305@fepi.local>
2023-11-29 19:23:23 -0300

    Mudanças lab 05

--- api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -1,64 +1,68 @@
 package br.fepi.socialbooks.resources;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController ;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.fepi.socialbooks.domain.Comentario;
 import br.fepi.socialbooks.domain.Livro;
+import br.fepi.socialbooks.repository.ComentarioRepository;
 import br.fepi.socialbooks.repository.LivrosRepository;
 
 @RestController
 @RequestMapping(value = "/livros")
-public class LivrosResources {
+public class LivrosResources { 
 	
 	@Autowired
 	private LivrosRepository livrosRepository;
-	
+
+	@Autowired
+	private ComentarioRepository comentariosRepository; 
+
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro>  listar() {
-		Livro livro1 = new Livro("a arte da guerra");
-		Livro livro2 = new Livro("O pequeno príncipe");
-		Livro livro3 = new Livro("1984");
-		
-		Livro[] livros = {
-			livro1, livro2,livro3
-		};
-		
-		return Arrays.asList(livros);
+	public List<Livro> listar() {
+		return livrosRepository.findAll();
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		
-		livrosRepository.save(livro);
+	@PostMapping()
+	public Livro salvar(@RequestBody Livro livro){
+		return livrosRepository.save(livro);
 	}
-	
-	//@RequestMapping(method = RequestMethod.GET)
-	//public List<Livro> listar(@RequestBody Livro livro) {
-	//	return livrosRepository.findAll();
-	//}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)	
-	public Livro buscaId(@PathVariable("id") long id) {
+	@CrossOrigin
+	@GetMapping(value = "/{id}")
+	public Livro findById(@PathVariable (value = "id")Long id){
 		return livrosRepository.findById(id).orElse(null);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)	
-	public void deletar (@PathVariable("id") Long id) {
+	@DeleteMapping(value = "/{id}")
+	public void deletar(@PathVariable(value = "id")Long id){
 		livrosRepository.deleteById(id);
 	}
 
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void atualizar (@RequestBody Livro livro,@PathVariable("id") Long id) {
+	@PutMapping(value = "{id}")
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
 		livro.setId(id);
 		livrosRepository.save(livro);
 	}
 	
+	@RequestMapping(value = "/{id}/comentario", method = RequestMethod.POST)
+	public void adicionarComentario(@PathVariable("id") Long livroId, @RequestBody Comentario comentario){
+		Livro livro = new Livro(null);
+		livro.setId(livroId);
+
+		comentario.setLivro(livro);
+		comentariosRepository.save(comentario);  
+	}
 }
